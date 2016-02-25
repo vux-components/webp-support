0a156d47dc9a3c83850c4af9b35745f4437da872
airyland <airyland@qq.com>
2016-02-25 13:04:19 +0800

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const webp_name = 'can_use_webp';
+function detectWebp() {
+  if (!window.localStorage || typeof localStorage !== 'object') return;
+  if (!localStorage.getItem(webp_name) || (localStorage.getItem(webp_name) !==
+      'available' && localStorage.getItem(webp_name) !== 'disable')) {
+    const img = document.createElement('img');
+    img.onload = function() {
+      try {
+        localStorage.setItem(webp_name, 'available');
+      } catch (ex) {}
+    };
+
+    img.onerror = function() {
+      try {
+        localStorage.setItem(webp_name, 'disable');
+      } catch (ex) {}
+    };
+    img.src =
+      'data:image/webp;base64,UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAsAAAABBxAREYiI/gcAAABWUDggGAAAADABAJ0BKgEAAQABABwlpAADcAD+/gbQAA==';
+  }
+}
+
+detectWebp();
+
+var supportWebp = function() {
+  return !!window.localStorage && window.localStorage.getItem(webp_name) === 'available';
+};
+
+module.exports = supportWebp
